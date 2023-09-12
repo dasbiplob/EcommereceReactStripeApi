437d673fce61ec0b31ce13a16d0df0957a7638df
dasbiplob <biplob90.d@git.com>
2023-09-12 19:38:20 +0300

    Developed the ProductCard and addition and substraction

--- onlinestore/src/CartContext.js
+++ onlinestore/src/CartContext.js
@@ -1,52 +1,93 @@
 import { createContext, useState } from "react";
-import { productList } from "./productsStore";
+import { getProductData } from "./ProductsStore.js";
 
-export const CartContext  = createContext({
+
+export const CartContext = createContext({
     items: [],
     getProductQuantity: () => {},
     addOneToCart: () => {},
     removeOneFromCart: () => {},
     deleteFromCart: () => {},
     getTotalCost: () => {}
-
 });
 
-export function CartProvider({childern}){
+export function CartProvider({children}) {
     const [cartProducts, setCartProducts] = useState([]);
+    
+    // [ { id: 1 , quantity: 3 }, { id: 2, quantity: 1 } ]
 
-    function getProductQuantity(id){
+    function getProductQuantity(id) {
         const quantity = cartProducts.find(product => product.id === id)?.quantity;
-        if(quantity === undefined){
+        
+        if (quantity === undefined) {
             return 0;
         }
+
         return quantity;
     }
 
-    function addOneToCart(id){
+    function addOneToCart(id) {
         const quantity = getProductQuantity(id);
-        if(quantity === 0){
+
+        if (quantity === 0) { // product is not in cart
             setCartProducts(
                 [
                     ...cartProducts,
                     {
-                        id:id,
-                        quantity:1
+                        id: id,
+                        quantity: 1
                     }
                 ]
-            ) 
-        }else {
+            )
+        } else { // product is in cart
+            // [ { id: 1 , quantity: 3 }, { id: 2, quantity: 1 } ]    add to product id of 2
             setCartProducts(
                 cartProducts.map(
                     product =>
-                    product.id === id
-                    ?{...product,quantity:product.quantity + 1}
-                    : product
+                    product.id === id                                // if condition
+                    ? { ...product, quantity: product.quantity + 1 } // if statement is true
+                    : product                                        // if statement is false
                 )
             )
         }
     }
 
-    function deleteFromCart(id){
+    function removeOneFromCart(id) {
+        const quantity = getProductQuantity(id);
+
+        if(quantity === 1) {
+            deleteFromCart(id);
+        } else {
+            setCartProducts(
+                cartProducts.map(
+                    product =>
+                    product.id === id                                // if condition
+                    ? { ...product, quantity: product.quantity - 1 } // if statement is true
+                    : product                                        // if statement is false
+                )
+            )
+        }
+    }
+
+    function deleteFromCart(id) {
+        // [] if an object meets a condition, add the object to array
+        // [product1, product2, product3]
+        // [product1, product3]
+        setCartProducts(
+            cartProducts =>
+            cartProducts.filter(currentProduct => {
+                return currentProduct.id !== id;
+            })  
+        )
+    }
+
+    function getTotalCost() {
+        let totalCost = 0;
+        cartProducts.forEach((cartItem) => {
+            const productData = getProductData(cartItem.id);
+            totalCost += (productData.price * cartItem.quantity);
+        });
+        return totalCost;
     }
 
     const contextValue = {
@@ -57,9 +98,18 @@ export function CartProvider({childern}){
         deleteFromCart,
         getTotalCost
     }
+
     return (
-        <CartContext.Provider value ={contextValue}>
-            {childern}
+        <CartContext.Provider value={contextValue}>
+            {children}
         </CartContext.Provider>
     )
 }
+
+export default CartProvider;
+
+
+// CODE DOWN HERE
+
+// Context (cart, addToCart, removeCart)
+// Provider -> gives your React app access to all the things in your context
--- onlinestore/src/component/productCard.js
+++ onlinestore/src/component/productCard.js
@@ -1,13 +1,32 @@
 import {Card, Button, Form, Row, Col} from "react-bootstrap"
+import { CartContext}   from '../CartContext.js'
+import { useContext } from "react";
+
 
 const ProductCard = (props) => {
     const product = props.product;
+    const cart = useContext(CartContext);
+    const productQuantity = cart.getProductQuantity(product.id);
+    //console.log(cart.items);
   return (
     <Card>
       <Card.Body>
         <Card.Title>{product.Title}</Card.Title>
         <Card.Title>€{product.price}</Card.Title>
-        <Button variant="primary">Add To Cart</Button>
+        { productQuantity > 0 ?
+        <>
+            <Form as={Row}>
+                            <Form.Label column="true" sm="6">In Cart: {productQuantity}</Form.Label>
+                            <Col sm="6">
+                                <Button sm="6" onClick={() => cart.addOneToCart(product.id)} className="mx-2">+</Button>
+                                <Button sm="6" onClick={() => cart.removeOneFromCart(product.id)} className="mx-2">-</Button>
+                            </Col>
+                        </Form>
+                        <Button variant="danger" onClick={() => cart.deleteFromCart(product.id)} className="my-2">Remove from cart</Button>
+        </>
+        :
+        <Button variant="primary" onClick={()=>cart.addOneToCart(product.id)}>Add To Cart</Button>
+        }
       </Card.Body>
     </Card>
   )
