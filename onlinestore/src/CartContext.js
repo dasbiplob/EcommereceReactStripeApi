d735a375cc96c63a8fd540edfece96aed58e8773
dasbiplob <biplob90.d@git.com>
2023-09-12 06:00:13 +0300

    working with Cart Context

---
+++ onlinestore/src/CartContext.js
@@ -0,0 +1,65 @@
+import { createContext, useState } from "react";
+import { productList } from "./productsStore";
+
+export const CartContext  = createContext({
+    items: [],
+    getProductQuantity: () => {},
+    addOneToCart: () => {},
+    removeOneFromCart: () => {},
+    deleteFromCart: () => {},
+    getTotalCost: () => {}
+
+});
+
+export function CartProvider({childern}){
+    const [cartProducts, setCartProducts] = useState([]);
+
+    function getProductQuantity(id){
+        const quantity = cartProducts.find(product => product.id === id)?.quantity;
+        if(quantity === undefined){
+            return 0;
+        }
+        return quantity;
+    }
+
+    function addOneToCart(id){
+        const quantity = getProductQuantity(id);
+        if(quantity === 0){
+            setCartProducts(
+                [
+                    ...cartProducts,
+                    {
+                        id:id,
+                        quantity:1
+                    }
+                ]
+            ) 
+        }else {
+            setCartProducts(
+                cartProducts.map(
+                    product =>
+                    product.id === id
+                    ?{...product,quantity:product.quantity + 1}
+                    : product
+                )
+            )
+        }
+    }
+
+    function deleteFromCart(id){
+    }
+
+    const contextValue = {
+        items: cartProducts,
+        getProductQuantity,
+        addOneToCart,
+        removeOneFromCart,
+        deleteFromCart,
+        getTotalCost
+    }
+    return (
+        <CartContext.Provider value ={contextValue}>
+            {childern}
+        </CartContext.Provider>
+    )
+}
